8f3e810aa06034afaa64831a51e395bedef5bf8f
avazbek489 <obidjonovavazbek787@gmail.com>
2024-09-27 20:42:01 +0500

    JS dan 11 ta savol tugadi

---
+++ script.js
@@ -0,0 +1,209 @@
+// // 1-SAVOL KODLARI
+
+// let num = +prompt("Sonni kiriting:");
+// if (num >= 0) {
+//     console.log("Musbat");
+// } else {
+//     console.log("Manfiy");
+// }
+
+
+
+
+
+
+
+// // 2-SAVOL KODLARI
+
+// let num = +prompt("Sonni kiriting:");
+// if (num % 2 == 0) {
+//     console.log("Juft");
+// } else {
+//     console.log("Toq");
+// }
+
+
+
+
+
+
+
+// // 3-SAVOL KODLARI
+
+// let num1 = +prompt("Birinchi sonni kiriting:");
+// let num2 = +prompt("Ikkinchi sonni kiriting:");
+
+// if (num1 > num2) {
+//     console.log("1-kiritilgan son 2-kiritilgan sondan katta");
+// } else if (num1 == num2) {
+//     console.log("Ikkala son teng");
+// } else {
+//     console.log("2-kiritilgan son 1-kiritilgan sondan katta");
+// }
+
+
+
+
+
+
+
+// // 4-SAVOL KODLARI
+
+// let tall = +prompt("Boyini kiriting:");
+// let span = +prompt("Enini kiriting:");
+
+// if( tall == span){
+//     console.log("Kvadrat");
+// } else{
+//     console.log("Tog'ri to'rtburchak");
+// }
+
+
+
+
+
+
+
+// // 5-SAVOL KODLARI
+
+// let temp = +prompt("Hozirgi haroratni kiriting😊");
+
+// if (temp > 0) {
+//     console.log("Hozir yoz harorati☀");
+// } else if (temp < 0) {
+//     console.log("Hozir qish harorati❄");
+// } else {
+//     console.log("Hozir harorat nolga teng🌡️");
+// }
+
+
+
+
+
+
+
+// // 6-SAVOL KODLARI
+
+// let grade = +prompt("O'quvchini baholang:");
+
+// if (grade === 5) {
+//     console.log("A'lo");
+// } else if (grade === 4) {
+//     console.log("Yaxshi");
+// } else if (grade === 3) {
+//     console.log("Qoniqarli");
+// } else if (grade === 2 || grade === 1) {
+//     console.log("Yomon");
+// } else {
+//     console.log("Noto'g'ri baho kiritildi");
+// }
+
+
+
+
+
+
+
+// // 7-SAVOL JAVOBI
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+
+// let res = num1;
+
+// if (num2 > res) {
+//     res = num2;
+// }
+
+// if (num3 > res) {
+//     res = num3;
+// }
+
+// console.log(res);
+
+
+
+
+
+
+
+// // 8-SAVOL KODLARI
+
+// let num = +prompt("Bironta istalgan sonni kiriting")
+
+// if (num >= 1000 && num <= 99999){
+//     console.log("Kiritilgan son 5xonali sondir");
+// } else{
+//     console.log("Kiritilgan son 5xonali son emas");
+
+// }
+
+
+
+
+
+
+
+// // 9-SAVOL KODLARI
+
+// let num = +prompt("Oyning raqamini kiriting:");
+
+// if (num >= 3 && num <= 5) {
+//     console.log("Bahor");
+// } else if (num >= 6 && num <= 8) {
+//     console.log("Yoz");
+// } else if (num >= 9 && num <= 11) {
+//     console.log("Kuz");
+// } else if (num === 12 || num === 1 || num === 2) {
+//     console.log("Qish");
+// } else {
+//     console.log("Noto'g'ri oy raqami kiritildi.");
+// }
+
+
+
+
+
+
+
+// // 10-SAVOL KODLARI
+
+// let day = +prompt("Hafta kunlaridan birini kiriting");
+
+// if (day >= 1 && day <= 5) {
+//     console.log("Ishchi kuni");
+// } else if (day >= 6 && day <= 7) {
+//     console.log("Dam olish kuni");
+// } else {
+//     console.log("Hato kun kiritildi");
+// }
+
+
+
+
+
+
+
+// // 11-SAVOL KODLARI
+
+// let young = +prompt("Yoshini kiriting:");
+// let year = 2024;
+
+// let birth = year - young;
+
+// console.log("Sizning yilingiz:", birth);
+
+// if (birth > 2005) {
+//   console.log("Yosh bola");
+// } else if (birth < 2005) {
+//   console.log("Katta odam");
+// } else {
+//   console.log("Hato kiritildi");
+// }
+
+
+
+
+// // 12-SAVOL KODLARI
+
